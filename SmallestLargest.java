a6193965f847a3ee68f14f432d3fac76749e53af
Vikram Raju <178485086+Vikram-Raju-1@users.noreply.github.com>
2024-08-21 19:44:51 +0000

    First commit (working solution)

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,31 @@
-//put your name here
+//Vikram Raju, 8/21/24
+
+import java.lang.Math;
+
 public class SmallestLargest{
     public static void main(String[] args){
-
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100) + 1;
+        } System.out.println(smallestValue(arr));
+        System.out.println(largestValue(arr));
     }
-    public static int smallestValue(){
-
+    public static int smallestValue(int[] arr){
+        int min = arr[0];
+        for (int i : arr) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
     }
-    public static int largestValue(){
-        
+    public static int largestValue(int[] arr){
+        int max = arr[0];
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
     }
 }
